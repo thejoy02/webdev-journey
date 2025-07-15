56f6cbc48f12ca57c728d9dd8833481b38f8ff6a
thejoy02 <sharmaanand1234x@gmail.com>
2025-07-15 23:18:03 +0530

    Add Day 23: JS Conditionals tasks.

---
+++ DAY23/script.js
@@ -0,0 +1,59 @@
+// console.log("Hello its Conditional Statements")
+// let grace =2;
+
+let age =0;
+
+
+if (age==18) {
+    console.log("Can Drive");
+}
+else if (age==0) {
+    console.log("U kidding?");
+}
+
+else {
+    console.log("Cant Drive");
+}
+
+let a = 5 , b=7 ;
+let c = a>b ?(a+b) : (a-b)
+
+console.log(c)
+
+
+
+
+
+
+
+
+
+
+
+// Assignment Operatos
+
+// age+= grace
+// age-= grace
+// age*= grace
+// age/= grace
+// age%= grace
+// age**= grace
+// console.log(age);
+
+// Artithmetic Operators
+
+// console.log(age-grace);
+// console.log(age*grace);
+// console.log(age/grace);
+// console.log(age**grace);
+// console.log(age%grace);
+
+// if ((age+grace)>18) {
+//     console.log("You can Vote");
+// }
+
+// else {
+//     console.log("You can't vote");
+// }
+
+// console.log(age+grace)
