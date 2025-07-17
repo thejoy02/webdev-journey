e0ca058225855476eb765b558ea29db6415884dc
thejoy02 <sharmaanand1234x@gmail.com>
2025-07-17 22:48:42 +0530

    Uploaded DAY25 with JAVASCRIPT FUNCTIONS

---
+++ DAY25/task.js
@@ -0,0 +1,64 @@
+// TASK -1 
+// Write a function greetUser(name) that prints "Hello, Aanand!" if the user’s name is Aanand.
+
+// function greetUser(name) {
+//     console.log("Hello, " + name +"!")
+// }
+
+// greetUser("Aanand")
+
+// TASK -2
+// Create a function add(a, b) that returns the sum.
+
+// function add(a,b) {
+//     return(a+b)
+// }
+// add(4,5)
+
+// console.log("The sum is: " + add)
+
+// TASK -3
+// Write a function isEven(num) that returns true if the number is even, else false.
+
+// function isEven(num) {
+//     return (num % 2 === 0) 
+// }
+
+// console.log(isEven(5))
+// console.log(isEven(6))
+
+// TASK -4
+// Create a function factorial(n) that returns the factorial of a number using a loop.
+
+// function factorial(n) {
+//   let result = 1;
+//   for (let i = 1; i <= n; i++) {
+//     result *= i;
+//   }
+//   return result;
+// }
+
+// console.log(factorial(5)); 
+
+// TASK -5
+// Write a function maxOfThree(a, b, c) that returns the largest number.
+
+// function maxOfThree(a, b, c) {
+//     if (a >= b && a >= c) return(a);
+//     else if (b >= a && b >= c) return(b);
+//     else return(c);
+// }
+
+// console.log("The largest number is: " + maxOfThree(10,5,15))
+
+
+
+
+
+
+
+
+
+
+
+
