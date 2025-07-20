175037c2fad7da8b1246ddaeb320fd5a2cac2f2f
thejoy02 <sharmaanand1234x@gmail.com>
2025-07-20 23:00:30 +0530

    Added DAY28 on Strings

---
+++ DAY28/task.js
@@ -0,0 +1,36 @@
+// console.log("Aanand")
+
+// console.log("This is a STRING lecture")
+
+// let e = "Aanand";
+// console.log(e[0]+e[1])
+
+let a = "Sonu" + "Sood";
+// console.log(a[0]);
+// console.log(a[1]);
+// console.log(a[2]);
+// console.log(a[3]);
+// console.log(a[4]);
+// console.log(a[5]);
+
+// console.log(a.length)
+
+// Template Laterals
+// let real_name = "Gitesh"
+// let friend = "Rohan"
+// console.log("His name is " + real_name + " and his friend's name is " + friend)
+// console.log(`His name is ${real_name} and his friend's name is ${friend}.` )
+
+
+// Properties
+let b = "ShivamSh"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase()) 
+console.log(b.length) 
+console.log(b.slice(1, 5)) 
+console.log(b.slice(1)) 
+console.log(b.replace("Sh","777"))
+console.log(b.concat(a, "Aishwariya", "Rahul", "Priya"))
+
+
+// console.log(b)
