3c7f7ff9847e96eeba4abb3e8c8463772f8769c6
thejoy02 <sharmaanand1234x@gmail.com>
2025-07-21 23:34:58 +0530

    Added DAY29 on JS Practice Questions

---
+++ DAY29/functions.js
@@ -0,0 +1,43 @@
+// 🟢 Functions
+// Create a function that multiplies 2 numbers.
+
+// Write an arrow function to check if a number is positive.
+
+// What happens if you don’t return in a function?
+
+// Difference between function declaration and expression?
+
+// Create a function that accepts a name and returns: “Hello, [name]!”
+
+// 1.
+// function multiply(x,y) {
+//     return(x*y);
+// }
+
+// console.log(multiply(2,5));
+
+// 2.
+
+// let num = (a) => {
+//     if (a > 0)
+//         console.log(`The number ${a} is positive`)
+//     else {
+//         console.log(`The number ${a} is negative`);
+//     }
+// }
+// console.log(num(-2));
+
+// 3.
+
+// it says undefined
+
+// 4.
+// I dont know these, please explain them
+
+// 5.
+
+let greet = (Name) => {
+    return (`Hello, ${Name}!`)
+}
+
+console.log(greet("Aanand"));
---
+++ DAY29/loops.js
@@ -0,0 +1,44 @@
+// Print all numbers from 1 to 10 using a for loop.
+
+// Sum all even numbers from 1 to 20.
+
+// What’s the difference between for...in and for...of?
+
+// Break the loop when number hits 7.
+
+// Reverse an array using a loop.
+
+// 1.
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// 2.
+
+// let sum = 0;
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 2 === 0) {
+//         sum += i;
+//     }
+// }
+// console.log("Sum of even numbers from 1 to 20 is:", sum);
+
+
+// 3. 
+
+// Difference b/w for in and for of loop is - for in loop focuses on keys of an object whereas for of loop focuses on values of thoses keys in an object
+
+// 4.
+
+// for (let i = 0; i <=7; i++) {
+//     if (i === 7)
+//         break;
+//     console.log(i)
+// }
+
+// 5.
+
+// let arr = ["tree", "apple", "head"];
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     console.log(arr[i]);
+// }
